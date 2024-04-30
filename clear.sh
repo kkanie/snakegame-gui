de4b84cf2c5bdf87add2a2458bda09b811d0c869
kkanie <116817352+kkanie@users.noreply.github.com>
2024-04-30 14:33:59 +0200

    Add files via upload

---
+++ clear.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$(docker ps -a -q)" ]; then
+  docker stop -f $(docker ps -a -q)
+  docker rm -f $(docker ps -a -q)
+fi
+
+if [ "$(docker images -q)" ]; then
+  docker rmi -f $(docker images -q)
+fi
+
+docker ps -a
+docker images
+
+if [! -d "log" ]; then
+ mkdir log
+fi
